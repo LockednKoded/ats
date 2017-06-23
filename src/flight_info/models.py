f5f4873adf3cb5e7b1404894ba1f69cf74de846b
LockedNCoded <koolkarishma95@gmail.com>
2017-06-23 16:22:34 +0530

    some more model modifications
    removed the Time model

--- src/flight_info/models.py
+++ src/flight_info/models.py
@@ -2,52 +2,40 @@
 from django.db import models
 from django.utils import timezone
 
-class Flights(models.Model):
-    flight_no = models.PositiveIntegerField()
+
+class Flight(models.Model):
+    flight_no = models.PositiveIntegerField(primary_key=True)
     airline = models.CharField(max_length=100)
     origin = models.CharField(max_length=100)
     destination = models.CharField(max_length=100)
     terminal = models.PositiveSmallIntegerField()
     concourse = models.PositiveSmallIntegerField()
     fare = models.FloatField()
-    capacity = models.PositiveIntegerField()
-    booked_seats =  models.PositiveIntegerField()
-    operation_days = models.CommaSeparatedIntegerField()
+    total_seats = models.PositiveIntegerField()
+    booked_seats = models.PositiveIntegerField()
+    operation_days = models.CommaSeparatedIntegerField(max_length=50)
     approved_plan = models.BooleanField(default=False)
+    status_delayed = models.BooleanField(default=False)
+    scheduled_arrival = models.DateTimeField(auto_now=False, auto_now_add=False)
+    scheduled_departure = models.DateTimeField(auto_now=False, auto_now_add=False)
+    revised_arrival = models.DateTimeField(auto_now=False, auto_now_add=False)
+    revised_departure = models.DateTimeField(auto_now=False, auto_now_add=False)
 
     def ___str__(self):
-        return self.title
+        return self.flight_no
+
 
 class Crew(models.Model):
-    name = models.CharField()
-    pilot_or_crew = models.BooleanField()  #False for Crew, True for pilot
-    id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    pilot = models.BooleanField(default=False)  # False for Crew, True for pilot
+    crew_id = models.IntegerField(primary_key=True)
     experience = models.PositiveSmallIntegerField()
     license = models.PositiveIntegerField()
     ph_no = models.BigIntegerField()
+    flight_link = models.ManyToManyField(Flight, blank=True)
 
     def ___str__(self):
-        return self.title
-
-class Time(models.Model):
-    flight_no = models.ForeignKey(Flight)
-    status_delayed =
-    scheduled_arrival =
-    scheduled_departure =
-    revised_arrival =
-    revised_departure =
-
-    def ___str__(self):
-        return self.title
-
-class FlightAandCrew(models.Model):
-    flight_no =
-    crew_id =
-
-
-
-    def ___str__(self):
-        return self.title
+        return self.name
 
 
 
