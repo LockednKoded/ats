1ab1c348479c4b29a57ae9081720cdb4552e3f98
Abhishek Nigam <abhisheknigam1996@gmail.com>
2017-06-27 23:29:11 +0530

    Made some changes to models.py

--- src/flight_info/models.py
+++ src/flight_info/models.py
@@ -1,6 +1,5 @@
 
 from django.db import models
-from django.utils import timezone
 
 
 class Flight(models.Model):
@@ -12,30 +11,36 @@ class Flight(models.Model):
     concourse = models.PositiveSmallIntegerField()
     fare = models.FloatField()
     total_seats = models.PositiveIntegerField()
-    booked_seats = models.PositiveIntegerField()
-    operation_days = models.CommaSeparatedIntegerField(max_length=50)
+    booked_seats = models.PositiveIntegerField(default=0)
     approved_plan = models.BooleanField(default=False)
+    operation_days = models.CharField(max_length=13)  # eg. 1,0,1,0,1,0,0 for a flight (M,W,F)
     status_delayed = models.BooleanField(default=False)
+
+    # Time attributes
     scheduled_arrival = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
     scheduled_departure = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
     revised_arrival = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
     revised_departure = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
+    time_last_updated = models.DateField(auto_now_add=True)
 
     def ___str__(self):
-        return self.flight_no
+        return self.flight_no + " - " + self.airline
 
 
 class Crew(models.Model):
+    crew_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     pilot = models.BooleanField(default=False)  # False for Crew, True for pilot
-    crew_id = models.IntegerField(primary_key=True)
-    experience = models.PositiveSmallIntegerField()
-    license = models.PositiveIntegerField()
+    experience = models.PositiveSmallIntegerField(default=0)
+    license_no = models.PositiveIntegerField()
     ph_no = models.BigIntegerField()
-    flight_link = models.ManyToManyField(Flight, blank=True)
+
+    # blank=True --> There may be unassigned crew, Crew table doesn't have total participation
+    flights = models.ManyToManyField(Flight, blank=True)
 
     def ___str__(self):
-        return self.name
 
-
-
+        if self.pilot:
+            return self.crew_id + " " + self.name + " - " + "Pilot"
+        else:
+            return self.crew_id + " " + self.name + " - " + "Crew"
