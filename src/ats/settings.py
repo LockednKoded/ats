dda261bd002cdc157e9faba4a63df172da31a29a
Abhishek Nigam <abhisheknigam1996@gmail.com>
2017-06-29 12:23:47 +0530

    Added cripy forms in settings
    Note: Pls install the package in your virtual environment using "pip install django-crispy-forms" and also "pip install Pillow" (for flight_info app) in your virtual environement.

--- src/ats/settings.py
+++ src/ats/settings.py
@@ -31,14 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+    'crispy_forms',
+
+    # Our apps
     'flight_info.apps.FlightInfoConfig',
-    'parking.apps.ParkingConfig'
+    'parking.apps.ParkingConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,15 +125,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
 ]
 
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 # BASE_DIR refers to dir where manage.py lives, but we want it to look in the directory of virtual env
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")  # stores files uploaded by user
+
+# For crispy forms
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
