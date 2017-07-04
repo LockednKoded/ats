177b0db1ae0886fef616cee644109d4664c41b74
iamrachitgulati <iamrachitgulati@users.noreply.github.com>
2017-07-04 17:05:58 +0530

    Changes to Parking Module(app)

--- src/parking/urls.py
+++ src/parking/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import (parking_details,)
+from .views import (parking_details, search)
 
 urlpatterns = [
-    url(r'^$', parking_details),
+    url(r'^$', parking_details, name='details'),
+    url(r'^search/', search, name='search'),
 ]
--- src/parking/views.py
+++ src/parking/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, render_to_response
+from .models import ParkingSpot, VehicleDetail
 # Create your views here.
 
 
 def parking_details(request):
-    return render(request, "parking/parking.html", {})
+
+    try:
+        spots = ParkingSpot.objects.latest('last_updated')
+    except (KeyError, ParkingSpot.DoesNotExist):
+        return render(request, "parking/parking.html", {})
+
+    return render(request, "parking/parking.html", {'spots': spots})
+
+
+def search(request):
+    try:
+        spots = ParkingSpot.objects.latest('last_updated')
+        context = {'spots': spots}
+    except (KeyError, ParkingSpot.DoesNotExist):
+        context = {}
+    try:
+        query = request.GET.get('vno')
+        if query != '\0':
+            result = VehicleDetail.objects.get(vehicle_no=query)
+            context.update({'result': result})
+    except (KeyError, VehicleDetail.DoesNotExist):
+            context.update({'no_match': 1})
+
+    return render(request, 'parking/parking.html', context)
