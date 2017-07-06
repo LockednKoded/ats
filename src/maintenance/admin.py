112d01fb5b5dcc4c3bb90477311ed8db9b1de289
Abhishek Nigam <abhisheknigam1996@gmail.com>
2017-07-06 22:55:57 +0530

    Inlined Worker model with User in django-admin

--- src/maintenance/admin.py
+++ src/maintenance/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Worker
 
-admin.site.register(Worker)
+
+class WorkerInline(admin.StackedInline):
+    model = Worker
+    can_delete = False
+    verbose_name_plural = 'Worker'
+    fk_name = 'user'
