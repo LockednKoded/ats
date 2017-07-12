a4caef3aad3cef6349d229c18a2423ce4267923e
Abhishek Nigam <abhisheknigam1996@gmail.com>
2017-07-12 20:00:07 +0530

    Minor change, corrected making approved_plan not required field

--- src/flight_info/forms.py
+++ src/flight_info/forms.py
@@ -6,7 +6,7 @@ from .models import Flight, Crew
 class FlightForm(forms.ModelForm):
 
     flight_no = forms.IntegerField(help_text="Enter a unique flight no", label="Flight No")
-    approved_plan = forms.BooleanField(help_text="Check if plan is approved", label="Plan approved")
+    approved_plan = forms.BooleanField(help_text="Check if plan is approved", label="Plan approved", required=False)
 
     class Meta:
         model = Flight
@@ -46,6 +46,27 @@ class FlightForm(forms.ModelForm):
             flights_qs = Flight.objects.get(flight_no=flight_no)
             if flights_qs.exists():
                 raise forms.ValidationError("Flight with same no. already exists")
+
+        # def clean__revised_arrival(self):
+        #     scheduled_arrival = self.cleaned_data.get('scheduled_arrival')
+        #     revised_arrival = self.cleaned_data.get('revised_arrival')
+        #
+        #     if revised_arrival < scheduled_arrival:
+        #         raise forms.ValidationError('Scheduled arrival should be before revised arrival')
+        #
+        # def clean__revised_departure(self):
+        #     scheduled_departure = self.cleaned_data.get('scheduled_departure')
+        #     revised_departure = self.cleaned_data.get('revised_departure')
+        #
+        #     if revised_departure < scheduled_departure:
+        #         raise forms.ValidationError('Scheduled departure should be before revised departure')
+
+        # def clean__booked_seats(self):
+        #     total_seats = self.cleaned_data.get('total_seats')
+        #     booked_seats = self.cleaned_data.get('booked_seats')
+        #
+        #     if booked_seats > total_seats:
+        #         raise forms.ValidationError('Booked seats cannot be greater than total no of seats')
 
 
 class CrewForm(forms.ModelForm):
