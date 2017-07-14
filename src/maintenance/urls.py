f75068903663eeeb15bf0462c4794af76cf9bd30
iamrachitgulati <iamrachitgulati@users.noreply.github.com>
2017-07-14 14:47:38 +0530

    Changes in Maintenance Module(app)

--- src/maintenance/urls.py
+++ src/maintenance/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from .views import *
+from .views import test_view, search_worker, assign_worker
 
 app_name = "maintenance"
 
 urlpatterns = [
 
     #  /maintenance/
-    url(r'^$', test_view, name="test-view"),
+    url(r'^$', test_view, name='test-view'),
+    url(r'^search$', search_worker, name='search_worker'),
+    url(r'^assign$', assign_worker, name='assign_worker'),
 ]
--- src/maintenance/views.py
+++ src/maintenance/views.py
@@ -1,6 +1,66 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Worker
+from django.core.exceptions import PermissionDenied
+from .forms import SearchForm, UserForm, AssignForm
 
 
 def test_view(request):
-    return HttpResponse("<h2>Maintenance view is working</h2>")
+
+    form = SearchForm(request.POST, request.FILES)
+    context = {'form': form, 'submit_message': 'Search'}
+
+    form = UserForm(request.POST, request.FILES)
+    context.update({'form2': form, 'submit_message2': 'Assign Work'})
+
+    return render(request, "maintenance/maintenance.html", context)
+
+
+def search_worker(request):
+
+    if request.user.is_superuser:
+        form = SearchForm(request.POST, request.FILES)
+        context = {'form': form, 'submit_message': 'Search'}
+
+        form = UserForm(request.POST, request.FILES)
+        context.update({'form2': form, 'submit_message2': 'Assign Work'})
+
+        try:
+            query = request.GET.get('worker_type')
+            if query != '\0':
+                result = Worker.objects.filter(worker_type=query)
+                context.update({'result': result})
+        except (KeyError, ValueError, Worker.DoesNotExist,):
+                return redirect("maintenance:test-view")
+
+        return render(request, 'maintenance/maintenance.html', context)
+
+    else:
+        raise PermissionDenied
+
+
+def assign_worker(request):
+
+    if request.user.is_superuser:
+
+        try:
+            query = request.GET.get('user')
+            if query != '\0':
+                result = Worker.objects.get(user=query)
+                form = AssignForm(request.POST, request.FILES, instance=result)
+                if request.method == "POST":
+                    if form.is_valid():
+                        spots = form.save(commit=False)
+                        spots.save()
+
+                        return redirect("maintenance:test-view")
+                else:
+                    form = AssignForm(instance=result)
+
+                return render(request, 'maintenance/maintenance_form.html', {'title_message': 'Assign Work to Worker',
+                                                                             'submit_message': 'Update',
+                                                                             'form': form})
+        except (KeyError, ValueError, Worker.DoesNotExist,):
+                return redirect("maintenance:test-view")
+
+    else:
+        raise PermissionDenied
