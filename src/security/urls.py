6a5c90d4410c7291c612d1c2f9de874406627149
jitesh77 <jitesh.gupta77@gmail.com>
2017-07-17 17:54:17 +0530

    temp
    tempo

--- src/security/urls.py
+++ src/security/urls.py
@@ -6,6 +6,26 @@ app_name = "security"
 urlpatterns = [
 
 
+# Flight Options:
+
+    # Homepage for flights, showing a list of all scheduled flights, /flights/   #done view
+    url(r'^$', list_flights, name="list-flights"),
+
+    # Option to add new flights, /flights/add/                   #done view
+    url(r'^add/$', add_flight, name="add-flight"),
+
+    # Option to view flight details, /flights/5/                    #done view
+    url(r'^(?P<pk>[0-9]+)/$', view_flight, name="view-flight"),
+
+    # Option to delete flight, /flights/5/delete/                       #done view
+    url(r'^(?P<pk>[0-9]+)/delete/$', delete_flight, name="delete-flight"),
+
+    # Option to edit flight details, /flights/5/edit                    #done view
+    url(r'^(?P<pk>[0-9]+)/edit/$', edit_flight, name="edit-flight"),
+
+
+
+
     # Force_employee Options:
 
     # Option to view all crew/pilots, /flights/crew
@@ -23,38 +43,23 @@ urlpatterns = [
     # Option to edit crew details, /flights/crew/5/edit
     url(r'^force_employee/(?P<pk>[0-9]+)/edit/$', edit_force_employee, name="edit-force_employee"),
 
-    # Crew Options:
 
-    # Option to view all crew/pilots, /flights/crew
-    url(r'^crew/$', list_force_employee(), name="list-crew"),
-
-    # Option to add crew, /flights/crew/add
-    url(r'^crew/add/$', add_force_employee(), name="add-crew"),
-
-    # Option to view crew details, /flights/crew/5
-    url(r'^crew/(?P<pk>[0-9]+)/$', view_force_employee(), name="view-crew"),
-
-    # Option to delete crew, /flights/crew/5/delete
-    url(r'^crew/(?P<pk>[0-9]+)/delete/$', delete_force_employee(), name="delete-crew"),
-
-    # Option to edit crew details, /flights/crew/5/edit
-    url(r'^crew/(?P<pk>[0-9]+)/edit/$', edit_force_employee(), name="edit-crew"),
 
 ]
 
 #security_forces
 
 # Option to view all crew/pilots, /flights/crew
-url(r'^crew/$', list_security_forces(), name="list-crew"),
+url(r'^security_forces()/$', list_security_forces(), name="list-security_forces()"),
 
 # Option to add crew, /flights/crew/add
-url(r'^crew/add/$', add_security_forces(), name="add-crew"),
+url(r'^security_forces()/add/$', add_security_forces(), name="add-security_forces()"),
 
 # Option to view crew details, /flights/crew/5
-url(r'^crew/(?P<pk>[0-9]+)/$', view_security_forces(), name="view-crew"),
+url(r'^security_forces()/(?P<pk>[0-9]+)/$', view_security_forces(), name="view-security_forces()"),
 
 # Option to delete crew, /flights/crew/5/delete
-url(r'^crew/(?P<pk>[0-9]+)/delete/$', delete_security_forces(), name="delete-crew"),
+url(r'^security_forces()/(?P<pk>[0-9]+)/delete/$', delete_security_forces(), name="delete-security_forces()"),
 
 # Option to edit crew details, /flights/crew/5/edit
-url(r'^crew/(?P<pk>[0-9]+)/edit/$', edit_security_forces(), name="edit-crew"),
+url(r'^security_forces()/(?P<pk>[0-9]+)/edit/$', edit_security_forces(), name="edit-security_forces()"),
