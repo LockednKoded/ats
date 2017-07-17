93942c766bbc283ee5b0e95d661a753f70903a08
Abhishek Nigam <abhisheknigam1996@gmail.com>
2017-07-17 20:28:19 +0530

    Added one more LinkedIn profile

--- src/ats/settings.py
+++ src/ats/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # Django apps
-    'book',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +49,7 @@ INSTALLED_APPS = [
     'cargo.apps.CargoConfig',
     'accounts.apps.AccountsConfig',
     'maintenance.apps.MaintenanceConfig',
+    'book.apps.BookConfig',
 ]
 
 MIDDLEWARE = [
