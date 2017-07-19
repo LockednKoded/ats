a71f666e2bbd0f5bc28e069206b844001a709bf9
Abhishek Nigam <abhisheknigam1996@gmail.com>
2017-07-19 12:18:01 +0530

    Some changes
    1. Added delete and back buttons to crew detail page
    2. Corrected form validation in airlines/flight/crew form
    3. Added authorization condition to show 'Add' button on list pages

--- src/flight_info/forms.py
+++ src/flight_info/forms.py
@@ -38,45 +38,47 @@ class FlightForm(forms.ModelForm):
             'revised_departure',
         ]
 
-        def clean(self):
-            scheduled_arrival = self.cleaned_data.get('scheduled_arrival')
-            scheduled_departure = self.cleaned_data.get('scheduled_departure')
-            revised_arrival = self.cleaned_data.get('revised_arrival')
-            revised_departure = self.cleaned_data.get('revised_departure')
+    def clean_flight_no(self):
+        flight_no = self.cleaned_data.get('flight_no')
 
-            if revised_arrival < scheduled_arrival:
-                raise forms.ValidationError('Scheduled arrival should be before revised arrival')
-            if revised_departure < scheduled_departure:
-                raise forms.ValidationError('Scheduled departure should be before revised departure')
-            return super(FlightForm, self).clean()
+        flights_qs = Flight.objects.filter(flight_no=flight_no)
+        if flights_qs.exists():
+            raise forms.ValidationError("Flight with this no already exists")
 
-        def clean__flight_no(self):
-            flight_no = self.cleaned_data.get('flight_no')
+        return flight_no
 
-            flights_qs = Flight.objects.get(flight_no=flight_no)
-            if flights_qs.exists():
-                raise forms.ValidationError("Flight with same no. already exists")
+    def clean_days_operational(self):
+        days_operational = self.cleaned_data.get('days_operational')
 
-        # def clean__revised_arrival(self):
-        #     scheduled_arrival = self.cleaned_data.get('scheduled_arrival')
-        #     revised_arrival = self.cleaned_data.get('revised_arrival')
-        #
-        #     if revised_arrival < scheduled_arrival:
-        #         raise forms.ValidationError('Scheduled arrival should be before revised arrival')
-        #
-        # def clean__revised_departure(self):
-        #     scheduled_departure = self.cleaned_data.get('scheduled_departure')
-        #     revised_departure = self.cleaned_data.get('revised_departure')
-        #
-        #     if revised_departure < scheduled_departure:
-        #         raise forms.ValidationError('Scheduled departure should be before revised departure')
+        if len(days_operational) == 0:
+            raise forms.ValidationError('Flight should be scheduled for atleast one day')
 
-        # def clean__booked_seats(self):
-        #     total_seats = self.cleaned_data.get('total_seats')
-        #     booked_seats = self.cleaned_data.get('booked_seats')
-        #
-        #     if booked_seats > total_seats:
-        #         raise forms.ValidationError('Booked seats cannot be greater than total no of seats')
+        return days_operational
+
+    def clean(self):
+        scheduled_arrival = self.cleaned_data.get('scheduled_arrival')
+        scheduled_departure = self.cleaned_data.get('scheduled_departure')
+        revised_arrival = self.cleaned_data.get('revised_arrival')
+        revised_departure = self.cleaned_data.get('revised_departure')
+
+        if revised_arrival < scheduled_arrival:
+            raise forms.ValidationError('Scheduled arrival should be before revised arrival')
+        if revised_departure < scheduled_departure:
+            raise forms.ValidationError('Scheduled departure should be before revised departure')
+
+        total_seats = self.cleaned_data.get('total_seats')
+        booked_seats = self.cleaned_data.get('booked_seats')
+
+        if booked_seats > total_seats:
+            raise forms.ValidationError('Booked seats cannot be greater than total seats')
+
+        origin = self.cleaned_data.get('origin')
+        destination = self.cleaned_data.get('destination')
+
+        if origin == destination:
+            raise forms.ValidationError('Origin and destination for flight cannot be same')
+
+        return super(FlightForm, self).clean()
 
 
 class CrewForm(forms.ModelForm):
@@ -101,12 +103,14 @@ class CrewForm(forms.ModelForm):
             'flights',
         ]
 
-        def clean__crew_id(self):
-            crew_id = self.cleaned_data.get('crew_id')
+    def clean_crew_id(self):
+        crew_id = self.cleaned_data.get('crew_id')
 
-            crew_qs = Crew.objects.get(crew_id=crew_id)
-            if crew_qs.exists():
-                raise forms.ValidationError("Crew with same id already exists")
+        crew_qs = Crew.objects.filter(crew_id=crew_id)
+        if crew_qs.exists():
+            raise forms.ValidationError("Crew with same id already exists")
+
+        return crew_id
 
 
 class AirlineForm(forms.ModelForm):
@@ -124,9 +128,20 @@ class AirlineForm(forms.ModelForm):
             'info',
         ]
 
-        def clean__flight_prefix(self):
-            flight_prefix = self.cleaned_data.get('flight_prefix')
+    def clean_flight_prefix(self):
+        flight_prefix = self.cleaned_data.get('flight_prefix')
 
-            airline_qs = Airline.objects.get(flight_prefix=flight_prefix)
-            if airline_qs.exists():
-                raise forms.ValidationError("Airline with same prefix already exists")
+        airline_qs = Airline.objects.filter(flight_prefix=flight_prefix)
+        if airline_qs.exists():
+            raise forms.ValidationError("Airline with same prefix already exists")
+
+        return flight_prefix
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+
+        airline_qs = Airline.objects.filter(name=name)
+        if airline_qs.exists():
+            raise forms.ValidationError("Airline with same name already exists")
+
+        return name
