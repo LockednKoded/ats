9edeb2f368342ac3ac4c04c29a6f71080f3d6681
Rachit <iamrachitgulati@users.noreply.github.com>
2017-07-24 21:08:45 +0530

    Update base.py

--- src/ats/settings/base.py
+++ src/ats/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'n8b%z+wkbmy-jogezai()1zc3br$epz6lfy&zo5$-of9)pa51$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['10.0.0.8', '127.0.0.1', 'atsproject.pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'atsproject.pythonanywhere.com', 'localhost']
 
 # Application definition
 
